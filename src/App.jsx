f11d9f6ba61d98a3e9532aee9728c172ce966c1d
josiesauceda <josie.sauceda@my.utsa.edu>
2025-10-26 11:12:56 -0500

    Pushing to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import Todo from './Todo'
-import Pethouse from './Pethouse'
-import { Routes, Route } from 'react-router-dom'
+import "./App.css";
+import Todo from "./Todo";
+import Pethouse from "./Pethouse";
+import Home from "./home/home.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Todo />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/pethouse" element={<Pethouse />} />
     </Routes>
-  )
+  );
 }
 
 export default App;
